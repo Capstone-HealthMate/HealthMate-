32ca8fd426835a31db3cdb34108ee0586d1ee6fa
Mochaimin Muhamad Rizq <90722468+MochaiminMR@users.noreply.github.com>
2024-06-18 13:11:05 +0700

    Update GreaseCalcu.jsx

--- front-end/src/components/layout/calculator/GreaseCalcu.jsx
+++ front-end/src/components/layout/calculator/GreaseCalcu.jsx
@@ -45,6 +45,15 @@ export default function GreaseCalcu() {
       })
 
       modalRef.current.showModal()
+
+      setUmur('')
+      setBeratBadan('')
+      setTinggiBadan('')
+      setUmur('')
+
+      setPangkalPaha('')
+      setLebarPinggang('')
+      setPanjangLeher('')
     } catch (error) {
       setErrorMessage(error.message)
       setShowToast(true)
@@ -52,7 +61,7 @@ export default function GreaseCalcu() {
   }
 
   const greaseCalculator = data.calculator.filter(
-    (data) => data.title !== "Tingkat Aktivitas Fisik"
+    (data) => data.title !== "Aktivitas Fisik"
   )
 
   return (
@@ -72,31 +81,31 @@ export default function GreaseCalcu() {
                         <h3 className="text-sm font-medium text-[#b9b9b9]">
                           Pilih Jenis Kelamin mu!
                         </h3>
-                        <div className="flex items-center justify-between">
-                          <span className="text-2xl text-secondary font-semibold">
+                        <div className="flex lg:flex-row flex-col items-center justify-between">
+                          <span className="lg:text-2xl text-base pb-2 text-secondary font-semibold">
                             {item.title}
                           </span>
-                          <div className="flex gap-4 px-8">
+                          <div className="flex gap-4 lg:px-8">
                             <div className="form-control flex flex-col justify-center items-center">
-                              <img className="w-20 h-20" src={iconBoy} alt="" />
+                              <img className="lg:w-20 w-16" src={iconBoy} alt="" />
                               <label className="label cursor-pointer space-x-2">
                                 <input
                                   type="radio"
                                   name="gender"
-                                  className="radio radio-sm radio-info"
+                                  className="radio lg:radio-sm radio-xs radio-info"
                                   required
                                   checked={gender === "laki-laki"}
                                   value="laki-laki"
                                   onChange={onGenderChange}
                                 />
-                                <span className="label-text text-lg">
+                                <span className="label-text lg:text-lg text-sm">
                                   Laki Laki
                                 </span>
                               </label>
                             </div>
                             <div className="form-control flex flex-col justify-center items-center">
                               <img
-                                className="w-20 h-20"
+                                className="lg:w-20 w-16"
                                 src={iconGirl}
                                 alt=""
                               />
@@ -105,12 +114,12 @@ export default function GreaseCalcu() {
                                   type="radio"
                                   name="gender"
                                   required
-                                  className="radio radio-sm radio-info"
+                                  className="radio lg:radio-sm radio-xs radio-info"
                                   checked={gender === "perempuan"}
                                   value="perempuan"
                                   onChange={onGenderChange}
                                 />
-                                <span className="label-text text-lg">
+                                <span className="label-text lg:text-lg text-sm">
                                   Perempuan
                                 </span>
                               </label>
@@ -127,7 +136,7 @@ export default function GreaseCalcu() {
                 case 5:
                 case 6:
                   return (
-                    <div className="col-span-3" key={index}>
+                    <div className="lg:col-span-3 col-span-full" key={index}>
                       <Card
                         valueInput={
                           index === 1
@@ -172,13 +181,13 @@ export default function GreaseCalcu() {
 
       {result && (
         <dialog ref={modalRef} id="my_modal_4" className="modal">
-          <div className="modal-box text-lg w-6/12 max-w-5xl flex flex-col text-center justify-center items-center">
+          <div className="modal-box text-lg lg:w-6/12 w-10/12 max-w-5xl flex flex-col text-center justify-center items-center">
             <div className="flex flex-col mb-2">
-              <h3 className="font-bold text-2xl">
+              <h3 className="font-bold lg:text-2xl text-lg">
                 Persentase Lemak Tubuh kamu: {result.bodyFatPercentage}%
               </h3>
-              <h2 className="font-bold text-xl">Tips Untuk Kamu</h2>
-              <ul>
+              <h2 className="font-bold lg:text-xl text-lg">Tips Untuk Kamu</h2>
+              <ul className="lg:text-base text-sm">
                 {result?.suggestions.map((item, index) => (
                   <li key={index}>{item}</li>
                 ))}
@@ -186,7 +195,7 @@ export default function GreaseCalcu() {
             </div>
             <div className="modal-action">
               <form method="dialog">
-                <button className="btn btn-secondary w-full">
+                <button className="btn lg:text-base text-sm font-medium btn-secondary w-full">
                   Hitung Lagi
                 </button>
               </form>
